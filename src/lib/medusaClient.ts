bc635ae8efacdaec0f2c159534185fbfa738c766
Soumil Arora <soumil789@gmail.com>
2025-06-22 19:21:02 +0530

    Removed CORS for stores

--- src/lib/medusaClient.ts
+++ src/lib/medusaClient.ts
@@ -1,10 +1,9 @@
 import Medusa from "@medusajs/medusa-js"
 
-const BASE = import.meta.env.VITE_MEDUSA_BACKEND_URL;
 const API_KEY  = import.meta.env.VITE_MEDUSA_PUBLISHABLE_API_KEY;
 
 export const medusa = new Medusa({
-  baseUrl: BASE,
+  baseUrl: "",
   maxRetries: 3,
   publishableApiKey: API_KEY,
 })
