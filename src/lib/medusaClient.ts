ce173460efb25f4fbbd35f85164b2ed1b4f7ac43
Soumil Arora <soumil789@gmail.com>
2025-07-01 11:38:28 +0530

    testing baseURL

--- src/lib/medusaClient.ts
+++ src/lib/medusaClient.ts
@@ -3,7 +3,7 @@ import Medusa from "@medusajs/medusa-js"
 const API_KEY  = import.meta.env.VITE_MEDUSA_PUBLISHABLE_API_KEY;
 
 export const medusa = new Medusa({
-  baseUrl: "",
+  baseUrl: "https://api.3deality.in",
   maxRetries: 3,
   publishableApiKey: API_KEY,
 })
