d645f338bb156534a4a3a8e5af2195dd57c92416
Soumil Arora <soumil789@gmail.com>
2025-07-16 01:51:39 +0530

    Back to CORS

--- src/lib/medusaClient.ts
+++ src/lib/medusaClient.ts
@@ -1,9 +1,10 @@
 import Medusa from "@medusajs/medusa-js"
 
 const API_KEY  = import.meta.env.VITE_MEDUSA_PUBLISHABLE_API_KEY;
+const base_url = import.meta.env.VITE_MEDUSA_BACKEND_URL;
 
 export const medusa = new Medusa({
-  baseUrl: "",
+  baseUrl: base_url,
   maxRetries: 3,
   publishableApiKey: API_KEY,
 })
